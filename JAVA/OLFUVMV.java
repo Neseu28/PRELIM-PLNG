d54b5b3d30409de4796b2004115c948edc9a9a77
GUERRERO NESTOR REY C. <ncguerrero0873val@student.fatima.edu.ph>
2026-09-17 16:33:20 +0800

    Add files via upload

---
+++ JAVA/OLFUVMV.java
@@ -0,0 +1,29 @@
+public class OLFUVMV {
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println("      OUR LADY OF FATIMA UNIVERSITY");
+        System.out.println("========================================");
+
+        System.out.println("\nVISION");
+        System.out.println("A premier inclusive university of choice aspiring");
+        System.out.println("to improve man as man by developing individuals");
+        System.out.println("through a legacy of excellent education and");
+        System.out.println("compassionate value formation.");
+
+        System.out.println("\nMISSION");
+        System.out.println("The Our Lady of Fatima University, together with");
+        System.out.println("Fatima Medical Science Foundation, Inc., is");
+        System.out.println("dedicated to the improvement of man as man");
+        System.out.println("through the holistic formation of individuals");
+        System.out.println("imbued with knowledge, skills, and virtues.");
+
+        System.out.println("\nCORE VALUES");
+        System.out.println("1. Respect");
+        System.out.println("2. Integrity");
+        System.out.println("3. Service");
+        System.out.println("4. Excellence");
+
+        System.out.println("\n========================================");
+    }
+}
